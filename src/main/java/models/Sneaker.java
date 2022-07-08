ed66b1a0ef7a6f79d0272be940ba72760178a622
Troy <troy@ZipcodersMBP28.fios-router.home>
2022-07-08 13:06:25 -0400

    added sneaker objects

--- src/main/java/models/Sneaker.java
+++ src/main/java/models/Sneaker.java
@@ -1,4 +1,10 @@
 package models;
 
 public class Sneaker {
+    private int id;
+    private String name;
+    private String brand;
+    private String sport;
+    private int size;
+    private float price;
 }
