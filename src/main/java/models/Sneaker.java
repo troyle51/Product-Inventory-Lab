1293a9879c815a9b6bc840ddd99396312d43613e
Troy <troy@ZipcodersMBP28.fios-router.home>
2022-07-10 07:08:59 -0400

    sport test

--- src/main/java/models/Sneaker.java
+++ src/main/java/models/Sneaker.java
@@ -7,4 +7,27 @@ public class Sneaker {
     private String sport;
     private int size;
     private float price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+    public String getSport() {
+        return sport;
+    }
+
+    public void setSport(String sport) {
+        this.sport = sport;
+    }
 }
--- src/test/java/models/SneakerTest.java
+++ src/test/java/models/SneakerTest.java
@@ -1,4 +1,46 @@
 package models;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class SneakerTest {
+
+    @Test
+    public void setNameTest() {
+        // given (1)
+        String expected = "OZWEEGO";
+
+        // when (2)
+        Sneaker testSneaker = new Sneaker();
+        testSneaker.setName(expected);
+
+        // then (3)
+        Assertions.assertEquals(expected, testSneaker.getName());
+    }
+
+    @Test
+    public void setBrandTest() {
+        // given (1)
+        String expected = "Nike";
+
+        // when (2)
+        Sneaker testSneaker = new Sneaker();
+        testSneaker.setBrand(expected);
+
+        // then (3)
+        Assertions.assertEquals(expected, testSneaker.getBrand());
+    }
+
+    @Test
+    public void setSportTest() {
+        // given (1)
+        String expected = "Tennis";
+
+        // when (2)
+        Sneaker testSneaker = new Sneaker();
+        testSneaker.setSport(expected);
+
+        // then (3)
+        Assertions.assertEquals(expected, testSneaker.getSport());
+    }
 }
